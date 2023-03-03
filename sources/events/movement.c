0e2e948f3d5a3b5ad6cffdb7964a72515632c381
Charline Dutel le vaillant <cdutel-l@z2r5p3.42lyon.fr>
2023-03-03 14:12:22 +0100

    a bit more opti for wall not too close

--- sources/events/movement.c
+++ sources/events/movement.c
@@ -6,22 +6,29 @@
 /*   By: cdutel-l <cdutel-l@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 11:35:41 by maxperei          #+#    #+#             */
-/*   Updated: 2023/03/03 12:11:36 by cdutel-l         ###   ########lyon.fr   */
+/*   Updated: 2023/03/03 13:40:42 by cdutel-l         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
 
-static void	check_wall_next_step(t_data *data, float new_x, float new_y)
+static void	check_wall_next_step(t_data *data, float new_x, float new_y, float next_x, float next_y)
 {
 	if ((int)new_x >= (int)data->config->x || (int)new_x < 0 \
-		|| (int)new_y >= (int)data->config->y || (int)new_y < 0)
+		|| (int)new_y >= (int)data->config->y || (int)new_y < 0\
+		|| (int)next_x >= (int)data->config->x || (int)next_x < 0 \
+		|| (int)next_y >= (int)data->config->y || (int)next_y < 0)
 		return ;
-	if (data->config->map[(int)new_y][(int)new_x] == '0' \
+	if ((data->config->map[(int)new_y][(int)new_x] == '0' \
 		|| data->config->map[(int)new_y][(int)new_x] == 'N'\
 		|| data->config->map[(int)new_y][(int)new_x] == 'E'\
 		|| data->config->map[(int)new_y][(int)new_x] == 'S'\
 		|| data->config->map[(int)new_y][(int)new_x] == 'W')
+		&& (data->config->map[(int)next_y][(int)next_x] == '0' \
+		|| data->config->map[(int)next_y][(int)next_x] == 'N'\
+		|| data->config->map[(int)next_y][(int)next_x] == 'E'\
+		|| data->config->map[(int)next_y][(int)next_x] == 'S'\
+		|| data->config->map[(int)next_y][(int)next_x] == 'W'))
 	{
 		data->player.x = new_x;
 		data->player.y = new_y;
@@ -32,38 +39,54 @@ void	dir_left(t_data *data)
 {
 	float	new_x;
 	float	new_y;
+	float	next_x;
+	float	next_y;
 
 	new_x = -sin(deg_to_rad(data->player.angle)) * 0.25f + data->player.x;
 	new_y = -cos(deg_to_rad(data->player.angle)) * 0.25f + data->player.y;
-	check_wall_next_step(data, new_x, new_y);
+	next_x = -sin(deg_to_rad(data->player.angle)) * 0.25f + new_x;
+	next_y = -cos(deg_to_rad(data->player.angle)) * 0.25f + new_y;
+	check_wall_next_step(data, new_x, new_y, next_x, next_y);
 }
 
 void	dir_right(t_data *data)
 {
 	float	new_x;
 	float	new_y;
+	float	next_x;
+	float	next_y;
 
 	new_x = sin(deg_to_rad(data->player.angle)) * 0.25f + data->player.x;
 	new_y = cos(deg_to_rad(data->player.angle)) * 0.25f + data->player.y;
-	check_wall_next_step(data, new_x, new_y);
+	next_x = sin(deg_to_rad(data->player.angle)) * 0.25f + new_x;
+	next_y = cos(deg_to_rad(data->player.angle)) * 0.25f + new_y;
+	check_wall_next_step(data, new_x, new_y, next_x, next_y);
 }
 
 void	dir_down(t_data *data)
 {
 	float	new_x;
 	float	new_y;
+	float	next_x;
+	float	next_y;
 
 	new_x = -cos(deg_to_rad(data->player.angle)) * 0.25f + data->player.x;
 	new_y = sin(deg_to_rad(data->player.angle)) * 0.25f + data->player.y;
-	check_wall_next_step(data, new_x, new_y);
+	next_x = -cos(deg_to_rad(data->player.angle)) * 0.25f + new_x;
+	next_y = sin(deg_to_rad(data->player.angle)) * 0.25f + new_y;
+	check_wall_next_step(data, new_x, new_y, next_x, next_y);
 }
 
 void	dir_up(t_data *data)
 {
 	float	new_x;
 	float	new_y;
+	float	next_x;
+	float	next_y;
 
 	new_x = cos(deg_to_rad(data->player.angle)) * 0.25f + data->player.x;
 	new_y = -sin(deg_to_rad(data->player.angle)) * 0.25f + data->player.y;
-	check_wall_next_step(data, new_x, new_y);
+	next_x = cos(deg_to_rad(data->player.angle)) * 0.25f + new_x;
+	next_y = -sin(deg_to_rad(data->player.angle)) * 0.25f + new_y;
+	check_wall_next_step(data, new_x, new_y, next_x, next_y);
 }
